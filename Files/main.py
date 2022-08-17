db4feca92f513fbba0ccfb3bbbe23de702560208
Goncalo-Chambel <goncalorrc@gmail.com>
2022-08-17 13:19:47 +0100

    Updating files

--- Files/main.py
+++ Files/main.py
@@ -4,15 +4,11 @@ from rotors import *
 
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def create_random_enigma_setup(nr_rotors):
 
-	plugboard = generate_random_plugboard_combination()
-	print("\nUsing the following plugboard: ",plugboard.values())
+def create_rotors(nr_rotors):
+
 	rotors = []
-	reflector = {}
-
 	rotors_used = "\nUsing the following rotor combinations:"
-	reflector_used = "\nUsing the following reflector:\n"
 	for i in range(nr_rotors):
 		rotor = {}
 		random_rotor_combination = random.choice(possible_rotor_combinations)
@@ -22,31 +18,45 @@ def create_random_enigma_setup(nr_rotors):
 
 		rotors.append(rotor)
 
+	print(rotors_used)
+	return rotors
+
+def create_reflector():
+
+	reflector = {}
+	reflector_used = "\nUsing the following reflector:\n"
 	random_reflector_combination = random.choice(possible_reflector_combinations)
 	reflector_used += random_reflector_combination
-	for i in range(len(random_rotor_combination)):
+	for i in range(len(random_reflector_combination)):
 		reflector[alphabet[i]] = random_reflector_combination[i]
 
-	print(rotors_used)
-	print(reflector_used+"\n")
+	print(reflector_used)
+	return reflector
+
+def create_random_enigma_setup(nr_rotors):
+
+	plugboard = generate_random_plugboard_combination()
+	print("\nUsing the following plugboard: ",plugboard.values())
+
+	rotors = create_rotors(nr_rotors)
+	reflector = create_reflector()
 
 	return plugboard, rotors, reflector
 
 def shift_values(rotor):
 	combination = list(rotor.values())
 	new_combination = np.roll(combination, 1)
-	#rotor = {**rotor, **dict(zip(rotor.keys, combination))}
 	for key, index in zip(rotor.keys(),range(len(rotor))):
 		rotor[key] = new_combination[index]
 
 	return rotor
+
 def update_rotors(rotors, rotors_rotation):
 
 	rotors_rotation[0] = rotors_rotation[0] + 1
 	rotors[0] = shift_values(rotors[0])
 	for i in range(len(rotors_rotation)):		
-		#if rotors_rotation[i] % len(rotor.values()) == 0:
-		if rotors_rotation[i] % 3 == 0 and rotors_rotation[i] != 0:
+		if rotors_rotation[i] % len(alphabet) == 0 and rotors_rotation[i] != 0:
 			rotors_rotation[i] = 0		
 			if i + 1 < len(rotors):
 				rotors_rotation[i+1] = rotors_rotation[i+1] + 1
@@ -95,12 +105,19 @@ def encode_letter(letter, plugboard, rotors, reflector):
 
 	return coded_letter
 
-def generate_random_plugboard_combination():
+def generate_random_plugboard_combination(nr_pairs = 13):
+
+	plugboard = {}
+
 	free_indexes = np.linspace(0,25,num=26, dtype='int')
 	final_combination = np.empty(len(alphabet),dtype='str')
-	_plugboard = {}
 
-	while free_indexes.size != 0:
+	for i in range(len(final_combination)):
+		final_combination[i] = alphabet[i]
+
+
+	while free_indexes.size != 0 and nr_pairs >= 0:
+		nr_pairs = nr_pairs - 1
 		index1 = random.choice(free_indexes)
 		free_indexes = np.delete(free_indexes, np.where(free_indexes == index1))
 		#print('index 1', index1)
@@ -112,17 +129,13 @@ def generate_random_plugboard_combination():
 		final_combination[index1] = alphabet[index2]
 		final_combination[index2] = alphabet[index1]
 
-		#print('Current combination', final_combination)
-	count = 0
-	for letter in alphabet:
-		_plugboard[letter] = final_combination[count]
-		count = count + 1
-
-	return _plugboard
+	for letter, index in zip(alphabet, range(len(alphabet))):
+		plugboard[letter] = final_combination[index]
+		
+	return plugboard
 
 
 if __name__ == '__main__':
-	random.seed(1)
 	nr_rotors = 3
 	rotors_rotation = []
 	for i in range(nr_rotors):
@@ -138,7 +151,4 @@ if __name__ == '__main__':
 			letter = letter.upper()
 			letter = encode_letter(letter, plugboard, rotors, reflector)
 			rotors = update_rotors(rotors, rotors_rotation)
-			print("Coded letter: ", letter)
-			#print(rotors_rotation)
-			#for rotor in rotors: 
-			#	print(list(rotor.values()))
+			print("Coded letter: ", letter) 
